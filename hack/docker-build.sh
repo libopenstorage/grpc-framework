d1b73919ac36bbf79ceebd6b8241ec9c0949be60
Luis Pabon <lpabon@purestorage.com>
2024-10-06 11:25:53 -0400

    oct 2024

--- hack/docker-build.sh
+++ hack/docker-build.sh
@@ -25,19 +25,19 @@ fi
 ## VERSIONS
 ## Confirm that the links are correct. Some tools change the links on newer versions
 # https://go.dev/dl/
-GFGOLANG=1.22.5
+GFGOLANG=1.23.2
 # https://github.com/grpc-ecosystem/grpc-gateway/releases
-GFGRPCGATEWAY=2.20.0
+GFGRPCGATEWAY=2.22.0
 # https://github.com/pseudomuto/protoc-gen-doc/releases
 GFPROTOCGENDOC=1.5.1
 # https://github.com/protocolbuffers/protobuf/releases
-GFPROTOC=27.2
+GFPROTOC=28.2
 
 # Get gRPC golang versions from here: https://grpc.io/docs/languages/go/quickstart/
 # Also see: https://pkg.go.dev/google.golang.org/protobuf/cmd/protoc-gen-go
 GFPROTOCGENGO=1.34.2
 # Also see: https://pkg.go.dev/google.golang.org/grpc/cmd/protoc-gen-go-grpc
-GFPROTOCGENGOGRPC=1.4.0
+GFPROTOCGENGOGRPC=1.5.1
 
 # Install tools from Ubuntu
 RUN apt-get -y -qq install \
